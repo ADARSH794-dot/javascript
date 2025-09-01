cbc3f157e8cccbd10634aebd43b8799ac04d1b94
ADARSH794-dot <adarshkuragr@gmail.com>
2025-09-01 07:23:29 +0000

    updated

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,2 +1,13 @@
-console.log("hitesh")
 console.log("adarsh")
+const ada=5;
+let x="sa";
+let y;
+let p=null;
+console.log(x);
+console.log(typeof x);
+console.log(ada);
+console.log(typeof ada);
+console.log(y);
+console.table([ada,x,y,p]);
+console.log (typeof p);
+console.log(3+3);
